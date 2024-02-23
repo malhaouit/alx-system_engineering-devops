e8ba40df69df7e57facd4c543cc8558c1579bfa0
malhaouit <abdelmounaim.abdel@gmail.com>
2024-02-23 14:38:01 -0800

    Update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,17 +1,7 @@
-# Install flask 2.1.0 using pip3
+# This manifest installs Flask version 2.1.0 using pip3
 
-package { 'python3-pip':
-  ensure => installed,
+package { 'Flask':
+ensure   => '2.1.0',             # Specify the version of the package to install
+provider => 'pip3',              # Specify the package provider
 }
 
-exec { 'install-flask':
-  command => 'pip3 install Flask==2.1.0',
-  unless  => 'pip3 freeze | grep Flask==2.1.0',
-  require => Package['python3-pip'],
-}
-
-exec { 'install-werkzeug':
-  command => 'pip3 install Werkzeug',
-  unless  => 'pip3 freeze | grep Werkzeug',
-  require => Exec['install-flask'],
-}
