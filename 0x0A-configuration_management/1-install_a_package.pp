025bb068243475434a8d88ba0ac85610a2817edc
malhaouit <abdelmounaim.abdel@gmail.com>
2024-02-23 14:18:21 -0800

    Update: change the package's name

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # This manifest installs Flask version 2.1.0 using pip3
 
-package { 'Flask':
+package { 'flask':
   ensure   => '2.1.0',             # Specify the version of the package to install
   provider => 'pip3',              # Specify the package provider
 }
