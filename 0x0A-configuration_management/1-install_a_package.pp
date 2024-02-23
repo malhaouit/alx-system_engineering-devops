7e26c82641bdfe359842113fed19f4afd1fdef8d
malhaouit <abdelmounaim.abdel@gmail.com>
2024-02-23 14:36:19 -0800

    Update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,17 @@
-#!/usr/bin/pup
-# Install flask version 2.1.0 using pip3
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+# Install flask 2.1.0 using pip3
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install-flask':
+  command => 'pip3 install Flask==2.1.0',
+  unless  => 'pip3 freeze | grep Flask==2.1.0',
+  require => Package['python3-pip'],
+}
+
+exec { 'install-werkzeug':
+  command => 'pip3 install Werkzeug',
+  unless  => 'pip3 freeze | grep Werkzeug',
+  require => Exec['install-flask'],
 }
