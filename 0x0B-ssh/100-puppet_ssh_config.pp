e63ab074e79246c9748970b82cf81789301cbd76
malhaouit <abdelmounaim.abdel@gmail.com>
2024-02-25 17:00:08 -0800

    Update: reduce puppet manifest content

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,25 +1,12 @@
-# Using Puppet to make changes to our configuration file.
+# Uses puppet to modify file configuration.
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
 
 file_line { 'Declare identity file':
-  ensure  => present,
-  path    => '/etc/ssh/ssh_config',
-  line    => 'IdentityFile ~/.ssh/school',
-  match   => '^IdentityFile',
-  replace => true,
-}
-
-file_line { 'Turn off passwd auth':
-  ensure             => present,
-  path               => '/etc/ssh/ssh_config',
-  line               => 'PasswordAuthentication no',
-  match              => '^PasswordAuthentication',
-  append_on_no_match => true,
-}
-
-file_line { 'Turn off challenge response auth':
-  ensure             => present,
-  path               => '/etc/ssh/ssh_config',
-  line               => 'ChallengeResponseAuthentication no',
-  match              => '^ChallengeResponseAuthentication',
-  append_on_no_match => true,
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
 }
