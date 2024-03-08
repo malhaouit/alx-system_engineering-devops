6b62fa64e2d964397870b45de54e48cb22fc2dbb
malhaouit <abdelmounaim.abdel@gmail.com>
2024-03-08 13:08:30 -0800

    update

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -4,17 +4,19 @@ package { 'nginx':
   ensure => installed,
 }
 
-file_line { 'custom_http_header':
-  ensure  => present,
-  path    => '/etc/nginx/sites-available/default',
-  line    => 'add_header X-Served-By $hostname;',
-  match   => '^add_header X-Served-By',
-  require => Package['nginx'],
-  notify  => Service['nginx'],
+$hostname = $facts['networking']['hostname']
+
+file_line { 'nginx_custom_header':
+    path    => '/etc/nginx/sites-available/default',
+    line    => "add_header X-Served-By '${hostname}';",
+    match   => '^(\s*)#?add_header X-Served-By',
+    replace => true,
+    require => Package['nginx'],
+    notify  => Service['nginx'],
 }
 
 service { 'nginx':
   ensure  => running,
   enable  => true,
-  require => File_line['custom_http_header'],
+  require => File_line['nginx_custom_header'],
 }
