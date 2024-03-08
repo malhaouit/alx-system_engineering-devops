85c01114516b653b81440e8c830883d2d7889233
malhaouit <abdelmounaim.abdel@gmail.com>
2024-03-08 11:45:48 -0800

    2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,17 +1,10 @@
 #!/usr/bin/env bash
-# Automates the task of creating a custom HTTP header response with Puppet.
-
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  require => Exec['append-custom-header'],
-}
+# Automates the task of creating a custom HTTP header response but Puppet.
 
 exec { 'append-custom-header':
-  command => "echo '\n	add_header X-Served-By \$hostname;' >> /etc/nginx/sites-available/default",
+  command => "echo '\n    add_header X-Served-By \$hostname;' >> /etc/nginx/sites-available/default",
   path    => ['/bin', '/usr/bin'],
+  unless  => "grep -Fxq '    add_header X-Served-By \$hostname;' /etc/nginx/sites-available/default",
   require => Package['nginx'],
   notify  => Service['nginx'],
 }
@@ -21,9 +14,14 @@ package { 'nginx':
 }
 
 service { 'nginx':
-  ensure     => running,
-  enable     => true,
-  hasrestart => true,
-  hasstatus  => true,
-  require    => Package['nginx'],
+  ensure => running,
+  enable => true,
+}
+
+file { '/etc/nginx/sites-available/default-permissions':
+  ensure => file,
+  path   => '/etc/nginx/sites-available/default',
+  owner  => 'root',
+  group  => 'root',
+  mode   => '0644',
 }
