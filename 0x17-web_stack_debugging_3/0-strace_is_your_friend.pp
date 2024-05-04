33a9b5d23b85cf84df136fb72a57c86a5967ca61
malhaouit <abdelmounaim.abdel@gmail.com>
2024-05-04 18:20:12 +0000

    Update: change the manifest

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,14 +1,7 @@
 # This manifest fixes the apache error occured withing the file wp-settings.php when requesting the apache server
 
-file_line { 'fix_class-wp-locale.phpp':
-  path   => '/var/www/html/wp-settings.php',
-  line   => "require_once( ABSPATH . WPINC . '/class-wp-locale.php' );",
-  match  => "^require_once\\( ABSPATH . WPINC . '/class-wp-locale.phpp' \\);",
-  notify => Service['apache2'],
-}
-
-service { 'apache2':
-  ensure    => running,
-  enable    => true,
-  subscribe => File_line['fix_class-wp-locale.phpp'],
+exec { 'replace-phpp-with-php':
+  command => "sed -i 's/\\.phpp/\\.php/g' /var/www/html/wp-settings.php",
+  path    => ['/usr/bin', '/bin'],
+  onlyif  => "grep -q '\\.phpp' /var/www/html/wp-settings.php",
 }
