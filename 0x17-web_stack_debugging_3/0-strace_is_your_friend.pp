d2677f7d2455455d15be865742928910c00ffe06
MALHAOUI ABDELMOUNAIM <abdelmounaim.abdel@gmail.com>
2024-05-06 17:46:28 +0100

    Delete 0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,7 +0,0 @@
-# This manifest fixes the apache error occured withing the file wp-settings.php when requesting the apache server
-
-exec { 'replace-phpp-with-php':
-  command => "sed -i 's/\\.phpp/\\.php/g' /var/www/html/wp-settings.php",
-  path    => ['/usr/bin', '/bin'],
-  onlyif  => "grep -q '\\.phpp' /var/www/html/wp-settings.php",
-}
