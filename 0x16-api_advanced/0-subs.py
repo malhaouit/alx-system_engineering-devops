d5679631e1e7b10e0c41e649757e9fbccdfdcda2
malhaouit <abdelmounaim.abdel@gmail.com>
2024-05-07 09:33:49 -0700

    Request Reddit API and return number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This module contains one function that requests a REDDIT API.
+File: 0-subs.py
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Requests a REDDIT API and returns returns the number of subscribers.
+    If an invalid subreddit is given, the function should return 0.
+    """
+    url = "https://www.reddit.com/r/{subreddit}/about.json".format(subreddit)
+    headers = {'User-Agent': 'My-Custom-Agent'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
+    else:
+        return 0
